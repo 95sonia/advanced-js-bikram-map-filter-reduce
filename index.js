6938fbb1b8aab5380da673acf9bc1568c51ffe36
95sonia <95sonia@gmail.com>
2025-10-22 16:34:01 +0200

    ADD: ejercicio 1

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 //RESUELVE LOS EJERCICIOS AQUI
+
+//1 Dado el array **numbers** cuyo valor sea [4, 5, 6, 7, 8, 9, 10], 
+// crea una función **elevados** que sea el resultado de elevar cada número a si mismo.
+
+numbers = [4, 5, 6, 7, 8, 9, 10];
+function elevados(numbers) {
+    return numbers**2;
+}
+
